922eca5e7787f629219b8b6ae7e487822d6dda80
carhenrodaff <affonso.carlos98@gmail.com>
2024-01-10 12:12:46 -0500

    Handlebar Templates

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+//Not best practice to read file every time, but for now it works
+//Better to read file once and store in variable(Cache)
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    pageTitle = process.env.npm_package_description + ' - Travel'
+    res.render('travel', { title: pageTitle, trips});
 };
 
 module.exports = {
