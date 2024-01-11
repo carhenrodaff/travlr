ecb051398a9f1b2d9819cfcaabb8067fe9f9f3fe
carhenrodaff <affonso.carlos98@gmail.com>
2024-01-11 15:25:21 -0500

    More HBS, basic api implemented

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = 'mongodb://${host}/travlr';
+const dbURI = `mongodb://${host}/travlr`;
 const readline = require('readline');
 
 mongoose.set('useUnifiedTopology', true);
 
 const connect = () => {
     setTimeout(() => mongoose.connect(dbURI, {
-        useNerUrlParser: true,
+        useNewUrlParser: true,
         useCreateIndex: true
     }), 1000);
     }
 
 mongoose.connection.on('connected', () => {
+    console.log('connected');
 });
 
 mongoose.connection.on('error', err => {
+    console.log('error: ' + err);
+    return connect();
 });
 
 mongoose.connection.on('disconnected', () => {
+    console.log('disconnected');
 });
 
 if (process.platform === 'win32') {
